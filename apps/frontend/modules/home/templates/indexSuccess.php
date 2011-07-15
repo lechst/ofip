06783f5ce44036d970164e21b8024b1ac6d8d92c
Lech Stawikowski <lech.stawikowski@gmail.com>
2011-07-15 12:03:47 +0200

    dodany layout dla 'home'

--- apps/frontend/modules/home/templates/indexSuccess.php
+++ apps/frontend/modules/home/templates/indexSuccess.php
@@ -1,1 +1,56 @@
-<h1>Homepage</h1>
+<div class="container_top">
+    
+    <div class="container_ofiptree">
+        
+        <div class="ofiptree_image"></div>
+        
+        <div class="menu_forms">
+            
+            <div class="box_form first"><span>ZGŁOŚ SIĘ</span></div>
+            <div class="box_form"><span>ZOSTAŃ WOLONTARIUSZEM</span></div>
+            <div class="box_form"><span>ZGŁOŚ TEMAT NA SESJĘ</span></div>
+            <div class="box_form"><span>WEŹ UDZIAŁ W KONFERENCJI</span></div>
+            <div class="box_form"><span>WEŹ UDZIAŁ W PIKNIKU</span></div>
+            
+        </div>
+        
+    </div>
+    
+    <div class="container_layoutimage"><img src="../images/layout_image.png" /></div>
+    
+</div>
+
+<div class="container_smallboxes">
+    
+    <div class="container_small first civil_week">
+        <img src="../images/small_image.png" />
+        <span class="title">Koncert <br /></span>
+        <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+    </div>
+    <div class="container_small ofip">
+        <span class="title">OFIP <br /></span>
+        <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus. Phasellus pharetra nulla ac diam.</span>
+    </div>
+    <div class="container_small last conference">
+        <span class="title">Konferencja <br /></span>
+        <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus. Phasellus pharetra nulla ac diam. Quisque semper justo at risus. Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui id orci. Nam congue, pede vitae dapibus aliquet, elit magna vulputate arcu, vel tempus metus leo non est. Etiam sit amet lectus quis est congue mollis. Phasellus congue lacus eget neque. Phasellus ornare, ante vitae consectetuer consequat, purus sapien ultricies dolor, et mollis pede metus eget nisi. Praesent sodales velit quis augue. Cras suscipit, urna at aliquam rhoncus, urna quam viverra nisi, in interdum massa nibh nec erat.</span>
+    </div>
+    
+    <br class="clear" />
+    
+</div>
+
+<div class="container_bigboxes">
+    
+    <div class="container_big first ofip">
+        <span class="title">OFIP jest cool! <br /></span>
+        <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus. Phasellus pharetra nulla ac diam. Quisque semper justo at risus. Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui id orci. Nam congue, pede vitae dapibus aliquet, elit magna vulputate arcu, vel tempus metus leo non est. Etiam sit amet lectus quis est congue mollis. Phasellus congue lacus eget neque. Phasellus ornare, ante vitae consectetuer consequat, purus sapien ultricies dolor, et mollis pede metus eget nisi. Praesent sodales velit quis augue. Cras suscipit, urna at aliquam rhoncus, urna quam viverra nisi, in interdum massa nibh nec erat.</span>
+    </div>
+    <div class="container_big picnic">
+        <span class="title">Zapraszamy na Piknik! <br /></span>
+        <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus. Phasellus pharetra nulla ac diam. Quisque semper justo at risus. Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui id orci.</span>
+    </div>
+    
+    <br class="clear" />
+    
+</div>

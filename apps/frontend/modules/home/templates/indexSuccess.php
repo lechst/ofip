e2d6890821e48851e117bf4df98d3272de0d2d36
Konrad Podgorski <konrad.podgorski@gmail.com>
2011-07-16 13:01:33 +0200

    poprawki z Laboratorium

--- apps/frontend/modules/home/templates/indexSuccess.php
+++ apps/frontend/modules/home/templates/indexSuccess.php
@@ -16,22 +16,29 @@
         
     </div>
     
-    <div class="container_layoutimage"><img src="../images/layout_image.png" /></div>
+    <div class="container_layoutimage">
+        <img id="im-1" src="images/layout_image.png" />
+        <img id="im-2" src="images/layout_image2.png" />
+        <img id="im-3" src="images/layout_image3.png" />
+        <img id="im-4" src="images/layout_image4.png" />
+        <img id="im-5" src="images/layout_image5.png" /> 
+    
+    </div>
     
 </div>
 
 <div class="container_smallboxes">
     
-    <div class="container_small first civil_week">
-        <img src="../images/small_image.png" />
+    <div id="ctnr-1" class="container_small first civil_week">
+        <img src="images/small_image.png" />
         <span class="title">Koncert <br /></span>
         <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
     </div>
-    <div class="container_small ofip">
+    <div id="ctnr-2" class="container_small ofip">
         <span class="title">OFIP <br /></span>
         <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus. Phasellus pharetra nulla ac diam.</span>
     </div>
-    <div class="container_small last conference">
+    <div id="ctnr-3" class="container_small last conference">
         <span class="title">Konferencja <br /></span>
         <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus. Phasellus pharetra nulla ac diam. Quisque semper justo at risus. Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui id orci. Nam congue, pede vitae dapibus aliquet, elit magna vulputate arcu, vel tempus metus leo non est. Etiam sit amet lectus quis est congue mollis. Phasellus congue lacus eget neque. Phasellus ornare, ante vitae consectetuer consequat, purus sapien ultricies dolor, et mollis pede metus eget nisi. Praesent sodales velit quis augue. Cras suscipit, urna at aliquam rhoncus, urna quam viverra nisi, in interdum massa nibh nec erat.</span>
     </div>
@@ -42,11 +49,11 @@
 
 <div class="container_bigboxes">
     
-    <div class="container_big first ofip">
+    <div id="ctnr-4" class="container_big first ofip">
         <span class="title">OFIP jest cool! <br /></span>
         <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus. Phasellus pharetra nulla ac diam. Quisque semper justo at risus. Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui id orci. Nam congue, pede vitae dapibus aliquet, elit magna vulputate arcu, vel tempus metus leo non est. Etiam sit amet lectus quis est congue mollis. Phasellus congue lacus eget neque. Phasellus ornare, ante vitae consectetuer consequat, purus sapien ultricies dolor, et mollis pede metus eget nisi. Praesent sodales velit quis augue. Cras suscipit, urna at aliquam rhoncus, urna quam viverra nisi, in interdum massa nibh nec erat.</span>
     </div>
-    <div class="container_big picnic">
+    <div id="ctnr-5" class="container_big picnic">
         <span class="title">Zapraszamy na Piknik! <br /></span>
         <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus. Phasellus pharetra nulla ac diam. Quisque semper justo at risus. Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui id orci.</span>
     </div>
--- web/js/mainscript.js
+++ web/js/mainscript.js
@@ -45,6 +45,111 @@ $(document).ready(function () {
     
     //jQuery Logotypes Hover
     
-    $(".logo_1").hover(function(){$(".logo_1").attr(src,"../images/logotype2_1.png")},function(){$(".logo_1").attr(src,"../images/logotype_1.png")});
+    $(".logo_1").hover(function(){
+        $(".logo_1").attr("src","images/logotype2_1.png")
+        },function(){
+        $(".logo_1").attr("src","images/logotype_1.png")
+        });
+    $(".logo_2").hover(function(){
+        $(".logo_2").attr("src","images/logotype2_2.png")
+        },function(){
+        $(".logo_2").attr("src","images/logotype_2.png")
+        });
+    $(".logo_3").hover(function(){
+        $(".logo_3").attr("src","images/logotype2_3.png")
+        },function(){
+        $(".logo_3").attr("src","images/logotype_3.png")
+        });
+    $(".logo_4").hover(function(){
+        $(".logo_4").attr("src","images/logotype2_4.png")
+        },function(){
+        $(".logo_4").attr("src","images/logotype_4.png")
+        });
+    $(".logo_5").hover(function(){
+        $(".logo_5").attr("src","images/logotype2_5.png")
+        },function(){
+        $(".logo_5").attr("src","images/logotype_5.png")
+        });
+    $(".logo_6").hover(function(){
+        $(".logo_6").attr("src","images/logotype2_6.png")
+        },function(){
+        $(".logo_6").attr("src","images/logotype_6.png")
+        });
+    $(".logo_7").hover(function(){
+        $(".logo_7").attr("src","images/logotype2_7.png")
+        },function(){
+        $(".logo_7").attr("src","images/logotype_7.png")
+        });
+    $(".logo_8").hover(function(){
+        $(".logo_8").attr("src","images/logotype2_8.png")
+        },function(){
+        $(".logo_8").attr("src","images/logotype_8.png")
+        });
+    $(".logo_9").hover(function(){
+        $(".logo_9").attr("src","images/logotype2_9.png")
+        },function(){
+        $(".logo_9").attr("src","images/logotype_9.png")
+        });
+    $(".logo_10").hover(function(){
+        $(".logo_10").attr("src","images/logotype2_10.png")
+        },function(){
+        $(".logo_10").attr("src","images/logotype_10.png")
+        });
+    $(".logo_11").hover(function(){
+        $(".logo_11").attr("src","images/logotype2_11.png")
+        },function(){
+        $(".logo_11").attr("src","images/logotype_11.png")
+        });
+     
+    // Fixing containers on mainpage height
+     
+    var height1 = Math.max($('#ctnr-1').height(),$('#ctnr-2').height(),$('#ctnr-3').height());
+    $('#ctnr-1').height(height1);
+    $('#ctnr-2').height(height1);
+    $('#ctnr-3').height(height1);
+    
+    var height2 = Math.max($('#ctnr-4').height(),$('#ctnr-5').height());
+    $('#ctnr-4').height(height2);
+    $('#ctnr-5').height(height2);
+    
+    // slideshow on main page
+    
+    ssStayTime = 4000;
+    ssTransTime = 2000;
+    
+    ssImgs = new Array();
+    ssActual = 0;
+    
+    $('.container_layoutimage img').each(function(index) {
+        ssImgs.push($(this));
+        $(this).css('opacity','0');
+    }   
+    );
+    ssImgs[0].css('opacity','1');
+    ssNumber = ssImgs.length;
+    
+    
+    
+    ssStep();        
+    
+    function ssStep() {
+  
+        ssImgs[(ssActual+1)%ssNumber].delay(ssStayTime).animate({
+            opacity: 1
+        },ssTransTime, function() {      
+            ssActual = (ssActual+1)%ssNumber;
+            
+            ssStep();
+                       
+        });
+        
+        ssImgs[ssActual].delay(ssStayTime).animate({
+            opacity: 0
+        },ssTransTime);
+  
+    }
+    
+    
+    
      
 });
